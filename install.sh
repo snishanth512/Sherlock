acc0a463d85ceef292abe13f7cacadc31f1445cb
snishanth512 <snishanth512@outlook.com>
2015-09-17 00:10:03 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ error_exit(){
 	exit 1
 	}
 
-npm install
+
 
 echo "Enter Game ID :  "
 read GAME_ID
@@ -17,7 +17,15 @@ echo ""
 
 echo '{"gameid":"'${GAME_ID}'"}' > game.json
 
-cat game.json
+sudo mkdir -p /home/pi/RRP
+[ $? -ne 0 ] && error_exit "error: make directory failed"
+
+sudo cp -rf ./* /home/pi/RRP/
+[ $? -ne 0 ] && error_exit "error: copy failed"
+
+cd /home/pi/RRP/
+
+npm install
 
 sudo chmod 0777 /home/pi/RRP/rrpd 
 [ $? -ne 0 ] && error_exit "error: rrpd file permission was not successful"
